eb225f69d46603e88343ebd7b56f511bfc56dc9c
stuianna <stuian@protonmail.com>
2019-02-21 16:11:32 +1000

    added sleep for VPN startup

--- polybar/myloc.sh
+++ polybar/myloc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Sleep here so VPN has time to start up
+sleep 10
 MYIP=$(curl 'https://ipinfo.io/ip' 2> /dev/null)
 LOC=$(echo $(geoiplookup "$MYIP") | sed -n 's/^.*: //p' 2> /dev/null)
 echo $LOC
