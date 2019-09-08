e900d1bef3e139ed9ed4d466d20bc5ad1a607fc3
Stuart Ianna <stuian@protonmail.com>
2019-09-08 12:10:16 +0300

    Added automute function

--- bin/spotify.sh
+++ bin/spotify.sh
@@ -1,32 +1,34 @@
 #!/bin/sh
 
 main() {
-  if ! pgrep -x spotify >/dev/null; then
-    echo ""; exit
-  fi  
+    if ! pgrep -x spotify >/dev/null; then
+        echo ""; exit
+    fi  
 
-  cmd="org.freedesktop.DBus.Properties.Get"
-  domain="org.mpris.MediaPlayer2"
-  path="/org/mpris/MediaPlayer2"
+    cmd="org.freedesktop.DBus.Properties.Get"
+    domain="org.mpris.MediaPlayer2"
+    path="/org/mpris/MediaPlayer2"
 
-  meta=$(dbus-send --print-reply --dest=${domain}.spotify \
-    /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:${domain}.Player string:Metadata)
+    meta=$(dbus-send --print-reply --dest=${domain}.spotify \
+        /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:${domain}.Player string:Metadata)
 
-  artist=$(echo "$meta" | sed -nr '/xesam:artist"/,+2s/^ +string "(.*)"$/\1/p' | tail -1  | sed 's/\&/\\&/g' | sed 's#\/#\\/#g')
-  album=$(echo "$meta" | sed -nr '/xesam:album"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1| sed 's/\&/\\&/g'| sed 's#\/#\\/#g')
-  title=$(echo "$meta" | sed -nr '/xesam:title"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1 | sed 's/\&/\\&/g'| sed 's#\/#\\/#g')
+    artist=$(echo "$meta" | sed -nr '/xesam:artist"/,+2s/^ +string "(.*)"$/\1/p' | tail -1  | sed 's/\&/\\&/g' | sed 's#\/#\\/#g')
+    album=$(echo "$meta" | sed -nr '/xesam:album"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1| sed 's/\&/\\&/g'| sed 's#\/#\\/#g')
+    title=$(echo "$meta" | sed -nr '/xesam:title"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1 | sed 's/\&/\\&/g'| sed 's#\/#\\/#g')
 
-  ad="Advertisement"
-  
+    ad="Advertisement"
+    num=$(pactl list | grep -E '(^Sink Input)|(media.name = \"Spotify\"$)' | awk '/Spotify/ {print a} {a = $0}' | cut -c 13-)
 
-  if [ $title == $ad ]
-  then
-      echo "Ad Blocked"
-      exit
-  fi
+    if [ $title == $ad ]
+    then
+        echo "Ad Blocked"
+        pactl set-sink-input-mute $num yes
+        exit
+    else
+        pactl set-sink-input-mute $num no
+    fi
 
-  echo "${*:-%artist% - %title%}" | sed "s/%artist%/$artist/g;s/%title%/$title/g;s/%album%/$album/g"i | sed "s/\&/\&/g" | sed "s#\/#\/#g" | cut -c 1-35
-
+    echo "${*:-%artist% - %title%}" | sed "s/%artist%/$artist/g;s/%title%/$title/g;s/%album%/$album/g"i | sed "s/\&/\&/g" | sed "s#\/#\/#g" | cut -c 1-35
 }
 
 main "$@"
