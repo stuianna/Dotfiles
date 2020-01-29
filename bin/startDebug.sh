67ca9c74f62e0f52831b4d070ebf25159afec162
Stuart Ianna <stuian@protonmail.com>
2020-01-29 21:07:27 +0200

    More work on vim debug

--- bin/startDebug.sh
+++ bin/startDebug.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 GDB=arm-none-eabi-gdb
-OPENOCD_INTERFACE=interface/stlink-v2-1.cfg
-OPENOCD_TARGET=target/stm32f7x.cfg
-BINARY="bin/project.elf"
+BINARY=$(find bin/ -name *.elf)
 
-${GDB} ${BINARY} -ex "target remote | openocd -f ${OPENOCD_INTERFACE} -f ${OPENOCD_TARGET} -c 'gdb_port pipe'" -ex "monitor reset halt" 
+OPENOCD_INTERFACE=$(sed  -n 's/OPENOCD_INTERFACE=\(\)/\1/p' .debugConf)
+OPENOCD_TARGET=$(sed  -n 's/OPENOCD_TARGET=\(\)/\1/p' .debugConf)
+OPENOCD_TRANSPORT=$(sed  -n 's/OPENOCD_TRANSPORT=\(\)/\1/p' .debugConf)
+OPENOCD_SCRIPT=$(sed  -n 's/OPENOCD_SCRIPT=\(\)/\1/p' .debugConf)
 
+if [[ "$OPENOCD_INTERFACE" != "" ]]; then OPENOCD_INTERFACE="-f interface/${OPENOCD_INTERFACE}";fi
+if [[ "$OPENOCD_TARGET" != "" ]]; then OPENOCD_TARGET="-f target/${OPENOCD_TARGET}";fi
+if [[ "$OPENOCD_TRANSPORT" != "" ]]; then OPENOCD_TRANSPORT="-c ${OPENOCD_TRANSPORT}";fi
+if [[ "$OPENOCD_SCRIPT" != "" ]]; then OPENOCD_SCRIPT="-f ${OPENOCD_SCRIPT}";fi
+
+echo ${BINARY}
+
+${GDB} ${BINARY} -ex "target remote | openocd ${OPENOCD_INTERFACE} ${OPENOCD_INTERFACE} ${OPENOCD_TARGET} -c 'gdb_port pipe'" -ex "monitor reset halt"
+
+exit
